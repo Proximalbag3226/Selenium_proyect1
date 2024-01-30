5e1f3ef496272d39c41e5ab2ec5f526f43810956
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-01-29 23:16:28 -0600

    Methods to create the docx file

--- config/navigator.py
+++ config/navigator.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from config import constants
+from docx import Document
 
 #Create a class that will be used to manage the functions of the chrome windows
 class ChromeWindow(webdriver.Chrome):
@@ -18,7 +19,6 @@ class ChromeWindow(webdriver.Chrome):
         self.execute_script(f"window.open('{page}');")
         self.switch_to.window(self.window_handles[1])
 
-
 class Navigation(webdriver.Chrome):
     
     def __init__(self, teardown=False):
@@ -30,6 +30,9 @@ class Navigation(webdriver.Chrome):
     def __exit__(self, exc_type, exc_value, trace):
         if self.teardown:
             self.quit()
+    
+    def return_page(self):
+        self.back()
     def get_url(self):
         self.get(constants.jornada)
         self.maximize_window()
@@ -40,4 +43,19 @@ class Navigation(webdriver.Chrome):
         news_bttn.click()
     
     def title_news(self):
-        title = self.find_element_by_xpath('')        
+        title = self.find_element_by_xpath('')
+        
+class WordDocumentCreator:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.doc = Document()
+
+    def add_paragraph(self, text):
+        self.doc.add_paragraph(text)
+
+    def add_heading(self, text, level=1):
+        self.doc.add_heading(text, level=level)
+
+    def save_document(self):
+        self.doc.save(self.file_name)
+        print(f"Documento Word '{self.file_name}' creado y guardado correctamente.")
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from config.navigator import Navigation
 from config.navigator import ChromeWindow as cw
 from config.constants import universal
 
+print("The options that we have to obtain the most recent news are: ")
+
+print("1.La Jornada"
+    "\n2.-El Universal"
+    "\n3.-El Economista"
+    "\n4.-Diario oficial de la federacion")
+
+newspaper = input("Chose one option")
+
 with Navigation() as nv:
     nv.get_url()
     nv.latest_news()
